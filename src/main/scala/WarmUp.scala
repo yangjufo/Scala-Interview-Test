1b82b3493b5760ed9829cb9f8bb915e782361c5f
Jimmy <yangjufo@gmail.com>
2018-12-01 18:39:11 -0800

    update WarmUp.scala

--- src/main/scala/WarmUp.scala
+++ src/main/scala/WarmUp.scala
@@ -1,16 +1,41 @@
 object WarmUp {
-  def main(args:Array[String]) {
+  def main(args: Array[String]) {
     //read n from console
-    val n = scala.io.StdIn.readInt()
+    //val n = scala.io.StdIn.readInt()
+    val n = 10
     //print result
-    println(recur(n))
+    println(recursion(n))
+    println(dynamicProgramming(n))
+    println(power(n))
   }
 
   //recursive function
-  def recur(n: Int): Int =  {
+  def recursion(n: Int): Int = {10
     if (n == 0)
       1
     else
-      recur(n - 1) + recur(n - 1)
+      recursion(n - 1) + recursion(n - 1)
   }
+
+  //dynamic programming function
+  def dynamicProgramming(n: Int): Int = {
+    var dp = new Array[Int](n + 1)
+    dp(0) = 1
+    1 to n foreach { i => dp(i) = dp(i - 1) + dp(i - 1) }
+    dp(n)
+  }
+
+  //power function
+  def power(n: Int): Int = {
+    if (n == 0)
+      1
+    else {
+      val tmp = power(n / 2)
+      if (n % 2 != 0)
+        2 * tmp * tmp
+      else
+        tmp * tmp
+    }
+  }
+
 }
